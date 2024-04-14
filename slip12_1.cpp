014878d6d6d678235d1749cf7c99362529625618
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-14 10:19:39 +0530

    Update slip12_1.cpp

--- slip12_1.cpp
+++ slip12_1.cpp
@@ -3,6 +3,23 @@ Q.1 Write an MPI program to calculate sum and average of randomly generated 1000
 numbers (stored in array) on a cluster 
 */
 
+/*
+Q.1 Write an MPI program to calculate sum and average of randomly generated 1000  
+numbers (stored in array) on a cluster 
+*/
+/**
+ * For linux
+ * compilation : mpicc Ex2.c
+ * Run : mpiexec -n 4 ./Ex2
+ * --------------------------------------------------
+ * Install the MSMPI for windows 
+ * 
+ * For compilation of program on windows
+ * gcc Ex1.c -o Ex1.exe -I "C:\Program Files (x86)\Microsoft SDKs\MPI\Include" "C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x86\msmpi.lib"
+ * 
+ * run 
+ * mpiexec -n 2 Ex1.exe
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<mpi.h>
