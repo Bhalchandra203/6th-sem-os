62ef2d39263ed23e713d6a1036dd6fcfdeb3b39e
Bhalchandra khairnar <145736562+Bhalchandra203@users.noreply.github.com>
2024-04-14 09:57:51 +0530

    Update slip11_2.cpp

--- slip11_2.cpp
+++ slip11_2.cpp
@@ -12,26 +12,30 @@ numbers (stored in array) on a cluster (Hint: Use MPI_Reduce) [15]
 int main(int argc , char *argv[])
 {
     int size,rank;
-    int local_sum = 0;
+    int local_min = MAX;
     int local_arr[MAX];
-    int global_sum = 0;
+    int global_min = MAX;
 
     MPI_Init(&argc , &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
 
     srand(rank);
-    for(int i =0; i<MAX; i++){
+    for(int i =0; i<MAX; i++)
+    {
         local_arr[i] = rand()%100;
-        local_sum += local_arr[i];
+        if(local_min > local_arr[i])
+        {
+            local_min = local_arr[i];
+        }
     }
 
-    MPI_Reduce(&local_sum , &global_sum , 1 , MPI_INT , MPI_SUM ,0, MPI_COMM_WORLD);
-    printf("Process : %d  sum %d \n" , rank , local_sum);
+    MPI_Reduce(&local_min , &global_min , 1 , MPI_INT , MPI_MIN ,0, MPI_COMM_WORLD);
+    printf("Process : %d local min %d \n" , rank , local_min);
 
     if(rank == 0)
     {
-        printf("Total sum %d " , global_sum);
+        printf("Global min %d " , global_min);
     }
     MPI_Finalize();
     return 0;
